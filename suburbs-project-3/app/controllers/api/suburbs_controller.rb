bd9a5ae347fac0f86d027f53118d0a91824775a1
Akiva Szental <amszental@gmail.com>
2016-05-11 14:30:25 +1000

    changed space

--- suburbs-project-3/app/controllers/api/suburbs_controller.rb
+++ suburbs-project-3/app/controllers/api/suburbs_controller.rb
@@ -16,9 +16,6 @@ module Api
       suburbs = Suburb.all
       suburb_array = []
 
-
-
-
       suburbs.each do |suburb|
         if (suburb.median_price_unit < max_price && suburb.median_price_unit > min_price) || (suburb.median_price_house < max_price && suburb.median_price_house > min_price)
           suburb_array.push({suburbName: suburb.suburb_name, housePrice: suburb.median_price_house, geocode: suburb.geocode })
